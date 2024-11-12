e24350ed3e7116f053302a060c667f50296ae85a
altrek <kolomensky4848@gmail.com>
2024-11-12 21:06:00 +0300

    d

---
+++ src/main/java/dev/PlanningProject/PlanningProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.PlanningProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/dev/PlanningProject/entities/GroupEntity.java
@@ -0,0 +1,4 @@
+package dev.PlanningProject.entities;
+
+public class GroupEntity {
+}
