6a268bd92458e2ddec317e57bfc89361b5c2dc1f
altrek <kolomensky4848@gmail.com>
2024-11-12 23:54:28 +0300

    1

--- src/main/java/dev/PlanningProject/PlanningProjectApplication.java
+++ src/main/java/dev/PlanningProject/PlanningProjectApplication.java
@@ -1,13 +1,23 @@
 package dev.PlanningProject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PlanningProjectApplication {
 
+
+	private static Initializer initiator;
+
+	@Autowired
+	public void setInitialLouder(Initializer initiator) {
+		PlanningProjectApplication.initiator = initiator;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(PlanningProjectApplication.class, args);
+		initiator.initial();
 	}
 
 }
--- src/main/java/dev/PlanningProject/entities/GroupEntity.java
+++ src/main/java/dev/PlanningProject/entities/GroupEntity.java
@@ -1,4 +1,33 @@
 package dev.PlanningProject.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "groups")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class GroupEntity {
+
+    public GroupEntity(String name){
+        this.name = name;
+    };
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "group")
+    List<PurchaseEntity> purchases;
+
+
 }
